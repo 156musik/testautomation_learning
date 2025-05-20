8f0f067194bd987bebc2df7bd5f2952d9a38ef9f
156musik <156macbookair@YukikonoMacBook-Air.local>
2025-05-20 23:18:33 +0900

    リネーム・ファイル移動・不要なフォルダの削除

--- tests/step03_google_search.spec.ts
+++ tests/step03_google_search.spec.ts
@@ -2,8 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('Google検索ができる', async ({ page }) => {
   await page.goto('https://www.google.com');
-  await page.fill('input[name="q"]', 'Playwright');
-  await page.press('input[name="q"]', 'Enter');
+
+  // textarea を使うと成功する場合が多い
+  await page.fill('textarea[name="q"]', 'Playwright');
+  await page.keyboard.press('Enter');
+
+  await page.waitForURL(/search/);
   await expect(page).toHaveURL(/search/);
   await expect(page.locator('text=Playwright')).toBeVisible();
 });
